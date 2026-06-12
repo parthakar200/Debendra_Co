5c92d78516c8429c835d6f9b54c8aaf3d0aafec7
parthakar200 <parthasarathikar2002@gmail.com>
2026-06-12 17:29:49 +0530

    added

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'jeebanco_jwt_secret_2026';
+const JWT_SECRET = process.env.JWT_SECRET || 'debendraco_jwt_secret_2026';
 
 exports.protect = async (req, res, next) => {
   try {
