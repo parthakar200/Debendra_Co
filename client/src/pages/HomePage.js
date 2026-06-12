0f5471f9788e0f91447e7659c914b4afb8f38b37
parthakar200 <parthasarathikar2002@gmail.com>
2026-06-12 17:34:17 +0530

    lol

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -160,7 +160,7 @@ export default function HomePage() {
     <div>
       {/* For Search Engine Optimization */}
       <Helmet>
-        <title>Jeeban & Co. - Company Registration, GST, ITR, Trademark India</title>
+        <title>Debendra & Co. - Company Registration, GST, ITR, Trademark India</title>
         <meta name="description" content="India's trusted platform for company registration, GST filing, trademark, and income tax services. Based in Soro, Baleshwar, Bhubaneswar, Odisha." />
         <meta name="keywords" content="company registration Bhubaneswar, GST registration Odisha, ITR filing Baleshwar, Project Funding, trademark India" />
       </Helmet>
